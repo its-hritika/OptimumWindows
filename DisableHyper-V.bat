dd75c3b4e451881e0fe303556ffcdac89282292d
Ashutosh <ashutoshtips@gmail.com>
2024-06-14 18:07:22 +0530

    Update DisableHyper-V.bat
    - More Fixes.

--- DisableHyper-V.bat
+++ DisableHyper-V.bat
@@ -14,8 +14,15 @@ fltmc >nul 2>&1 || (
 :: Initialize environment
 setlocal EnableExtensions DisableDelayedExpansion
 
+:: ----------------------------------------------------------
+:: ----------------Disable "Hypervisor" feature-----------------
+:: ----------------------------------------------------------
 echo --- Disable "Hypervisor" feature
 bcdedit /set hypervisorlaunchtype off
+echo.
+Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Hypervisor
+:: ----------------------------------------------------------
+
 
 :: ----------------------------------------------------------
 :: ----------------Disable "Hyper-V" feature-----------------
@@ -23,6 +30,7 @@ bcdedit /set hypervisorlaunchtype off
 echo --- Disable "Hyper-V" feature
 :: Disable the "Microsoft-Hyper-V-All" feature
 PowerShell -ExecutionPolicy Unrestricted -Command "$featureName = 'Microsoft-Hyper-V-All'; $feature = Get-WindowsOptionalFeature -FeatureName "^""$featureName"^"" -Online -ErrorAction Stop; if (-Not $feature) {; Write-Output "^""Skipping: The feature `"^""$featureName`"^"" is not found. No action required."^""; Exit 0; }; if ($feature.State -eq [Microsoft.Dism.Commands.FeatureState]::Disabled) {; Write-Output "^""Skipping: The feature `"^""$featureName`"^"" is already disabled. No action required."^""; Exit 0; }; try {; Write-Host "^""Disabling feature: `"^""$featureName`"^""."^""; Disable-WindowsOptionalFeature -FeatureName "^""$featureName"^"" -Online -NoRestart -LogLevel ([Microsoft.Dism.Commands.LogLevel]::Errors) -WarningAction SilentlyContinue -ErrorAction Stop | Out-Null; } catch {; Write-Error "^""Failed to disable the feature `"^""$featureName`"^"": $($_.Exception.Message)"^""; Exit 1; }; Write-Output "^""Successfully disabled the feature `"^""$featureName`"^""."^""; Exit 0"
+dism /online /disable-feature /featurename:microsoft-hyper-v-all
 :: ----------------------------------------------------------
 
 
@@ -52,6 +60,11 @@ echo --- Disable "Hyper-V Module for Windows PowerShell" feature
 PowerShell -ExecutionPolicy Unrestricted -Command "$featureName = 'Microsoft-Hyper-V-Management-PowerShell'; $feature = Get-WindowsOptionalFeature -FeatureName "^""$featureName"^"" -Online -ErrorAction Stop; if (-Not $feature) {; Write-Output "^""Skipping: The feature `"^""$featureName`"^"" is not found. No action required."^""; Exit 0; }; if ($feature.State -eq [Microsoft.Dism.Commands.FeatureState]::Disabled) {; Write-Output "^""Skipping: The feature `"^""$featureName`"^"" is already disabled. No action required."^""; Exit 0; }; try {; Write-Host "^""Disabling feature: `"^""$featureName`"^""."^""; Disable-WindowsOptionalFeature -FeatureName "^""$featureName"^"" -Online -NoRestart -LogLevel ([Microsoft.Dism.Commands.LogLevel]::Errors) -WarningAction SilentlyContinue -ErrorAction Stop | Out-Null; } catch {; Write-Error "^""Failed to disable the feature `"^""$featureName`"^"": $($_.Exception.Message)"^""; Exit 1; }; Write-Output "^""Successfully disabled the feature `"^""$featureName`"^""."^""; Exit 0"
 :: ----------------------------------------------------------
 
+:: ----------------------------------------------------------
+:: -Disable "Hyper-V net Adapters" feature--
+:: ----------------------------------------------------------
+echo --- Disable "Hyper-V net adapters" feature
+get-netadapter|where-object {$_.interfacedescription -like "*hyper-v*"}|Disable-NetAdapter
 
 :: Pause the script to view the final state
 pause
